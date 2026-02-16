ecd2d38a5e9f928440b60e80777eedb5e28a2155
pokuri  karthik <karthikpokuri13@gmail.com>
2026-02-16 11:44:40 +0530

    changed app.jsx

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,7 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base:"/Counter-APP",
+  base:"/CounterAPP/",
   plugins: [
     react({
       babel: {
